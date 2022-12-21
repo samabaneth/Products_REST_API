3362833ceb4150cb2d5c4d336293e152d9391812
Samantha Baneth <sambaneth8@gmail.com>
2022-12-20 19:31:25 -0700

    Migrated Django Data into MySql

--- products_project/settings.py
+++ products_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&b_!4hshyu6)#j-vxol0)lo&j_yf2@6u^vuyyy56y%7j2m+t%x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,15 +68,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'products_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -121,3 +111,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from products_project.local_settings import *
+except ImportError:
+    pass
