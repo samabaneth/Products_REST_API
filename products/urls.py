d35c62e7714a14eb106088ba9f28a47ae7d57d33
Samantha Baneth <sambaneth8@gmail.com>
2022-12-22 13:15:21 -0700

    Created New View Function for Getting Products by ID

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.products_list)
+    path('', views.products_list),
+    path('<int:pk>/', views.product_detail),
+
 ]
